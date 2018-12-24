33f61d5361a97f5811aa5c1bba31c143db8a5a33
Will Grigg <willwgrigg@gmail.com>
2018-12-24 06:05:10 -0700

    Captions are saved in a (slightly) more legible format

--- WEBVTT PROJ/webvtt test.py
+++ WEBVTT PROJ/webvtt test.py
@@ -1,11 +1,20 @@
 import webvtt
 
+# I wanted the file to to open in 'Create' mode ('x').
+# This just ensures that I don't overwrite a pre-existing file.
+
 vtt_file_name = 'Messages For The Future'
-transcript = open(vtt_file_name+' transcript.txt', 'a')
+file = open(vtt_file_name+' transcript.txt', 'x')
 
+# Holds the normalized caption text
+text = ''
+
+# Appends each caption to the text variable
 for caption in webvtt.read(vtt_file_name+'.vtt'):
-##    print(caption.start)
-##    print(caption.end)
-    transcript.write(caption.text)
+    text += caption.text + ' '
 
-transcript.close()
+# Newlines are replaced w/ spaces to increase readability
+text = text.replace('\n', ' ')
+file.write(text)
+
+file.close()
